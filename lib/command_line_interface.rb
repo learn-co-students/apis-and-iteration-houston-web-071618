818055fd40e46bc04ac9739c4681089c51f73fdc
ELehotzky <Lehotzky.Erica@gmail.com>
2018-07-17 17:14:32 -0500

    done

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,16 @@
+require "JSON"
+require "rest-client"
+character_data = RestClient.get("https://swapi.co/api/people/1")
+
+JSON.parse(character_data)
+
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to the Star Wars API!"
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "Please enter a character to learn more: "
+  gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
