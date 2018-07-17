b0b8234f03ecb2d28df102e6f184d2c44819ab06
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-17 18:22:49 -0500

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,23 +1,23 @@
-require 'rest-client'
-require 'json'
-require 'pry'
+require "rest-client"
+require "json"
+require "pry"
 
 def get_character_movies_from_api(character)
+
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  all_characters = RestClient.get("http://www.swapi.co/api/people/")
   character_hash = JSON.parse(all_characters)
 
-  results = character_hash("results")
 
-  results.collect do |character_data|
-    if character_data["name"] == character
-      return films = character_data["films"]
-      
+  results = character_hash["results"]
+  films = []
+  results.each do |desired_character|
+    if desired_character["name"].downcase == character
+      return films = desired_character["films"]
     end
   end
+  nil
 
-  
-  
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -29,14 +29,27 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
-def parse_character_movies(films_hash)
+def parse_character_movies(films_array)
   # some iteration magic and puts out the movies in a nice list
+  counter = 0
+  films_array.collect do |film_urls|
+    counter += 1
+    all_films = RestClient.get(film_urls)
+    pursed_films = JSON.parse(all_films)
+    puts "#{counter}. #{pursed_films["title"]}"
+  end
 end
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  films_array = get_character_movies_from_api(character)
+  if films_array == nil
+    puts "#{character} is not on the list of actors."
+  else
+    parse_character_movies(films_array)
+  end
 end
+
+# show_character_movies("Luke Skywalker")
 
 ## BONUS
 
