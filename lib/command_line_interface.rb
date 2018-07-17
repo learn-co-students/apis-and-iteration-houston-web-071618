951a48cab57cbba5aa1c1dcd7517a59d16668a4c
pabloargueta <pabloargueta27@gmail.com>
2018-07-17 18:16:44 -0500

    Fixed other merge conflicts

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,5 +1,5 @@
 def welcome
-<<<<<<< HEAD
+
   # puts out a welcome message here!
   puts "Welcome! The force awaits."
 
@@ -9,13 +9,13 @@ def get_character_from_user
   puts "please enter a character"
 
   character = gets.chomp.downcase
-=======
+
   puts "Welcome to the World of Star Wars! May the Force Be With You"
 end
 
 def get_character_from_user
   puts "please enter a character:"
->>>>>>> master
+
   # use gets to capture the user's input. This method should return that input, downcased.
   input = gets.chomp.downcase
 end
